8a521e85dbd3809212769d51744573be0c7aa577
Matthew McLaughlin <Matthew McLaughlin>
2016-07-25 01:23:39 -0700

    tap and tilt scrolling put in, but running into conflicts with gyro removal

--- client/mobile.js
+++ client/mobile.js
@@ -1,5 +1,62 @@
+// Connect to imperio socket room
+imperio.emitRoomSetup(function(socket) {
+  console.log('socket', socket);
+  var rooms = socket.rooms || 'no rooms';
+  console.log('ROOMS AFTER MOBILE ROOM SETUP: ', rooms);
+});
+
 var scrollBar = document.getElementById('scroll-bar');
 var base = document.getElementById('base');
+
+imperio.gesture('pan', scrollBar, null, handleScrollPan);
+
+function handleScrollPan(event) {
+  event.scroll = true;
+  return event;
+}
+
+var scrollButton = document.getElementById('scroll-button');
+var beta = document.getElementById('scroll-beta');
+var gamma = document.getElementById('scroll-gamma');
+var zero = document.getElementById('scroll-zero');
+var diff = document.getElementById('difference');
+var zeroGyro = true;
+var zeroedGyroAngle = 0;
+var inScroll = false;
+
+imperio.gesture('press', scrollButton, scrollGyroOn);
+imperio.gesture('pressUp', scrollButton, scrollGyroOff);
+
+function scrollGyroOn() {
+  console.log('detected press, turning gyro on');
+  inScroll = true;
+  imperio.emitGyroscope.start(printScrollGyro, modGyro);
+}
+
+function modGyro(event) {
+  console.log('in mod gyro');
+  if (zeroGyro) {
+    zeroedGyroAngle = event.beta;
+    zeroGyro = false;
+  }
+  event.difference = event.beta - zeroedGyroAngle;
+  event.inScroll = inScroll;
+  return event;
+}
+
+function printScrollGyro(event) {
+  console.log('in print gyro');
+  beta.innerHTML = "beta: " + Math.round(event.beta);
+  zero.innerHTML = "zero: " + zeroedGyroAngle;
+  diff.innerHTML = "diff: " + event.difference;
+}
+
+function scrollGyroOff() {
+  console.log('detected pressUp, turning gyro OFF');  
+  zeroGyro = true;
+  inScroll = false;
+  imperio.emitGyroscope.remove(printScrollGyro);
+}
 
 var browserState = {
   iacto: {
@@ -61,17 +118,3 @@ function renderBase() {
 function removeBase() {
   base.style.display = 'none';
 }
-
-imperio.gesture('pan', scrollBar, null, handleScrollPan);
-
-function handleScrollPan(event) {
-  event.scroll = true;
-  return event;
-}
-
-// Connect to imperio socket room
-imperio.emitRoomSetup(function(socket) {
-  console.log('socket', socket);
-  var rooms = socket.rooms || 'no rooms';
-  console.log('ROOMS AFTER MOBILE ROOM SETUP: ', rooms);
-});
